6c9dcb9054f9281d23c64c7314a1a6ad65480042
alperaltuntas <alperaltuntas@gmail.com>
2026-01-17 00:29:34 -0700

    remove read_data and write_data from fms_io

--- fms/fms.F90
+++ fms/fms.F90
@@ -152,7 +152,7 @@ use       mpp_io_mod, only:  mpp_io_init, mpp_open, mpp_close,         &
                        do_cf_compliance
 
 use fms_io_mod, only : fms_io_init, fms_io_exit, field_size, &
-                       read_data, write_data, read_compressed, read_distributed, &
+                       read_data, read_compressed, &
                        open_namelist_file, open_restart_file, open_ieee32_file, close_file, &
                        get_domain_decomp, &
                        open_file, open_direct_file, get_mosaic_tile_grid, &
@@ -177,7 +177,7 @@ public :: open_namelist_file, open_restart_file, &
           open_file, open_direct_file
 
 ! routines for reading/writing distributed data
-public :: read_data, write_data, read_compressed, read_distributed
+public :: read_data, read_compressed
 public :: get_domain_decomp, field_size
 public :: get_global_att_value
 
